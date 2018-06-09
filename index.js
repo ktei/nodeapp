92ce674a527947ee96c4c6635599018e8c6efbe7
Rui Cheng <ktei2008@gmail.com>
2018-06-10 09:58:36 +1000

    updated response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send('Hello world! This is from an image in DockerHub');
 });
 
 const server = app.listen(3000, () => {
